a08df2fba180be36cdda270412f3b009f6d3455d
Vadym Stupakov <vadim.stupakov@gmail.com>
2018-10-24 21:20:42 +0300

    Added readonly permissions. Updated install of deps

--- ftp_server.py
+++ ftp_server.py
@@ -5,7 +5,6 @@ __author__ = "Vadym Stupakov"
 __maintainer__ = "Vadym Stupakov"
 __email__ = "vadim.stupakov@gmail.com"
 
-import pip
 
 for i in range(2):
     try:
@@ -14,7 +13,10 @@ for i in range(2):
         from pyftpdlib.servers import FTPServer
         break
     except ImportError:
-        pip.main(["install", "pyftpdlib", "--user"])
+        import subprocess
+        from pathlib import Path
+        requirements = Path(__file__).parent / "requirements.txt"
+        subprocess.call(["pip3", "install", "--user", "-r", requirements])
 
 import argparse
 from pathlib import Path
@@ -34,17 +36,26 @@ def get_public_ip():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("FTP server for file sharing")
+
     parser.add_argument("-u", "--user", type=str, required=True)
-    parser.add_argument("-ps", "--password", type=str, required=True)
+    parser.add_argument("-p", "--password", type=str, required=True)
+    parser.add_argument("-r", "--readonly", action="store_true")
     parser.add_argument("-d", "--dir", type=Path, default=Path().cwd())
-    parser.add_argument("-i", "--ip", type=str, default=get_private_ip())
-    parser.add_argument("-p", "--port", type=int, default=60000)
-    parser.add_argument("-pr", "--port_range", default=range(60001, 61001))
+    parser.add_argument("--ip", type=str, default=get_private_ip())
+    parser.add_argument("--port", type=int, default=60000)
+    parser.add_argument("--port_range", default=range(60001, 61001))
 
     args = parser.parse_args()
 
+    perm_read = "elr"
+    perm_write = "adfmw"
+    permissions = perm_read + perm_write
+
+    if args.readonly:
+        permissions = perm_read
+
     authorizer = DummyAuthorizer()
-    authorizer.add_user(args.user, args.password, str(args.dir), perm="elradfmwMT")
+    authorizer.add_user(args.user, args.password, str(args.dir), perm=permissions)
 
     handler = FTPHandler
     handler.passive_ports = args.port_range
