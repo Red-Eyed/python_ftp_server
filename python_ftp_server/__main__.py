261d226b2bd53b3f0c55798dd1fb3d0fbfd31a60
Vadym Stupakov <vadim.stupakov@gmail.com>
2021-12-01 19:34:54 +0200

    reverted back

---
+++ python_ftp_server/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Vadym Stupakov"
+__email__ = "vadim.stupakov@gmail.com"
+
+from python_ftp_server.ftp_server import main
+
+if __name__ == '__main__':
+    main()
