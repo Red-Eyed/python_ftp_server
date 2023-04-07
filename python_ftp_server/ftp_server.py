f2bc86f54cf28057afd836c9567d1335084d0409
Vadym Stupakov <vadim.stupakov@gmail.com>
2023-04-07 21:18:21 +0300

    fixed #2

--- python_ftp_server/ftp_server.py
+++ python_ftp_server/ftp_server.py
@@ -27,12 +27,17 @@ def generate_password(strength):
     passwd = "".join(random.choice(chars) for i in range(strength))
     return passwd
 
-
 def get_local_ip():
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        s.connect(("8.8.8.8", 80))
-        return s.getsockname()[0]
-
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('192.255.255.255', 1))
+        ip = s.getsockname()[0]
+    except:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
 
 def get_hostname():
     return socket.gethostname()
